07445c4b74e51801e8764f2a430bfd27e63eff89
Johan Quijano <johanqnms@gmail.com>
2020-11-13 00:10:57 -0500

    Angular Updated ii

--- src/app/components/noticias/noticias.component.spec.ts
+++ src/app/components/noticias/noticias.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NoticiasComponent } from './noticias.component';
 
@@ -6,7 +6,7 @@ describe('NoticiasComponent', () => {
   let component: NoticiasComponent;
   let fixture: ComponentFixture<NoticiasComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NoticiasComponent ]
     })
